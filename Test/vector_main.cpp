26dd1862c417bc142462d8992f1d7ed9f30c535f
elvinbabaev <elvinbabaev@yandex.ru>
2021-06-19 21:34:07 +0300

    vector проверки check_assign

--- Test/vector_main.cpp
+++ Test/vector_main.cpp
@@ -6,6 +6,27 @@
 #include "../Container/vector.hpp"
 #include <iostream>
 
+typedef std::vector<int> std_vector_int;
+typedef ft::vector<int> ft_vector_int;
+typedef std::vector<int>::iterator std_iterator_int;
+typedef ft::vector<int>::iterator ft_iterator_int;
+
+template<class T>
+void check_assign(std::vector<T> std_vector, ft::vector<T> ft_vector) {
+	typename std::vector<T>::iterator std_iterator = std_vector.begin();
+	typename ft::vector<T>::iterator ft_iterator = ft_vector.begin();
+	while (std_iterator != std_vector.end() && ft_iterator != ft_vector.end()) {
+		assert(*std_iterator == *ft_iterator);
+		std_iterator++;
+		ft_iterator++;
+		if ((std_iterator == std_vector.end() && ft_iterator != ft_vector.end())
+		    || (std_iterator != std_vector.end() && ft_iterator == ft_vector.end())) {
+			assert(false);
+			return;
+		}
+	}
+}
+
 void testBegin()
 {
 	std::vector<int> std_vector(4, 10);
@@ -25,14 +46,14 @@ void testBegin()
 		i_ft++;
 		i_std++;
 	}
-
+	check_assign(std_vector, ft_vector);
 	std::cout<<"size: "<<ft_vector.size()<<" == "<<std_vector.size()<<std::endl;
 	std::cout<<"capacity: "<<ft_vector.capacity()<<" == "<<std_vector.capacity()<<std::endl;
 	std::cout<<"max_size: "<<ft_vector.max_size()<<" == "<<std_vector.max_size()<<std::endl;
 	std::cout<<"\tCheck resize"<<std::endl;
 	std_vector.resize(7);
 	ft_vector.resize(7);
-
+	check_assign(std_vector, ft_vector);
  	i_std = std_vector.begin();
 	i_ft = ft_vector.begin();
 	while (i_ft != ft_vector.end() || i_std != std_vector.end())
@@ -45,6 +66,7 @@ void testBegin()
 		std_vector[i] = i;
 		ft_vector[i] = i;
 	}
+	check_assign(std_vector, ft_vector);
 	std::cout<<"Check reserve: ";
 	std::cout<<"Capacity before: "<<ft_vector.capacity()<<" == "<<std_vector.capacity()<<std::endl;
 	ft_vector.reserve(100);
@@ -57,13 +79,14 @@ void testBegin()
 	std::cout<<"\tCheck empty()"<<std::endl;
 	if ((ft_vector.empty()) == (std_vector.empty()))
 		std::cout<<"Success"<<std::endl;
-
+	check_assign(std_vector, ft_vector);
 
 
 	std::cout<<"\tCheck at()\n";
 	for (int i = 0; i < ft_vector.size() || i < std_vector.size(); ++i) {
 		std::cout<<std_vector.at(i)<<" == "<<ft_vector.at(i)<<std::endl;
 	}
+	check_assign(std_vector, ft_vector);
 	std::cout<<"\tCheck back, pop_back\n";
 	while (!ft_vector.empty() || !std_vector.empty())
 	{
@@ -71,18 +94,20 @@ void testBegin()
 		std_vector.pop_back();
 		ft_vector.pop_back();
 	}
-
+	check_assign(std_vector, ft_vector);
 	std::cout<<"Check ft_vector"<<std::endl;
 	ft_vector.assign(1, 10);
 	for (ft::vector<int>::iterator i = ft_vector.begin(); i != ft_vector.end() ; i++) {
 		std::cout<<*i<<" ";
 	}
+	check_assign(std_vector, ft_vector);
 	std::cout<<std::endl;
 	std::cout<<"Capacity : "<<ft_vector.capacity()<<" Sise : "<<ft_vector.size()<<std::endl;
 	ft_vector.assign(100, 5);
 	for (ft::vector<int>::iterator i = ft_vector.begin(); i != ft_vector.end() ; i++) {
 		std::cout<<*i<<" ";
 	}
+	check_assign(std_vector, ft_vector);
 	std::cout<<std::endl;
 	std::cout<<"Capacity : "<<ft_vector.capacity()<<" Sise : "<<ft_vector.size()<<std::endl;
 
@@ -111,6 +136,7 @@ void testBegin()
 	std_vector.front() = 20;
 	ft_vector.front() = 20;
 	std::cout<<"front : "<<std_vector.front()<<" == "<<ft_vector.front()<<std::endl;
+	check_assign(std_vector, ft_vector);
 }
 
 template<class T>
@@ -141,19 +167,24 @@ void testSecond(){
         }
         printer(std_vector, ft_vector);
 
+	    check_assign(std_vector, ft_vector);
         std::cout<<"\tTest insert\n";
         std_vector.insert(((std_vector.begin()++)), -5);
         ft_vector.insert(((ft_vector.begin()++)), -5);
         printer(std_vector, ft_vector);
 
-        std::vector<int>::iterator i1 = std_vector.begin();
+	    check_assign(std_vector, ft_vector);
+
+	    std::vector<int>::iterator i1 = std_vector.begin();
         ft::vector<int>::iterator i2 = ft_vector.begin();
         for (int i = 0; i < 5; ++i) {
             i1++;i2++;
         }
+
         std_vector.insert(i1, 999);
         ft_vector.insert(i2, 999);
         printer(std_vector, ft_vector);
+	    check_assign(std_vector, ft_vector);
         std::cout<<ft_vector.size()<<" == "<<std_vector.size()<<std::endl;
         std::cout<<ft_vector.capacity()<<" == "<<std_vector.capacity()<<std::endl;
 
@@ -167,44 +198,20 @@ void testSecond(){
         for (int i = 100; i < 105; ++i) {
             tmp.push_back(i);
         }
-
+	    check_assign(std_vector, ft_vector);
         std_vector.insert(i1, tmp.begin(), (--tmp.end()));
         ft_vector.insert(i2, tmp.begin(), (--tmp.end()));
         printer(std_vector, ft_vector);
         std::cout<<ft_vector.size()<<" == "<<std_vector.size()<<std::endl;
         std::cout<<ft_vector.capacity()<<" == "<<std_vector.capacity()<<std::endl;
+	    check_assign(std_vector, ft_vector);
     }
 }
 
 int main()
 {
-//	testBegin();
-//	testSecond();
-
-    std::vector<int> test;
-    for (int i = 0; i < 20; ++i) {
-        test.push_back(i);
-    }
-    std::cout<<test.size()<<" "<<test.capacity()<<std::endl;
-    std::cout<<"|"<<*(test.erase((++(++(++(test.begin())))), (--(--(--test.end())))))<<"|\n";
-    std::cout<<*(test.erase((++(++(++(test.begin()))))))<<"|\n";
-    std::cout<<test.size()<<" "<<test.capacity()<<std::endl;
-    for (std::vector<int>::iterator i = test.begin(); i != test.end() ; i++) {
-        std::cout<<*i<<" ";
-    }
-    std::cout<<std::endl;
-    ft::vector<int> test1;
-    for (int i = 0; i < 20; ++i) {
-        test1.push_back(i);
-    }
-    std::cout<<test1.size()<<" "<<test1.capacity()<<std::endl;
-    std::cout<<"|"<<*(test1.erase((++(++(++(test1.begin())))), (--(--(--test1.end())))))<<"|\n";
-    std::cout<<*(test1.erase((++(++(++(test1.begin()))))))<<"|\n";
-    std::cout<<test1.size()<<" "<<test1.capacity()<<std::endl;
-    for (ft::vector<int>::iterator i = test1.begin(); i != test1.end() ; i++) {
-        std::cout<<*i<<" ";
-    }
-    std::cout<<std::endl;
-
+	testBegin();
+	testSecond();
+	while (true){}
 	return (0);
 }
